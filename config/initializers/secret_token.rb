c49721512dca6162b16f4feaf3d609006a172c28
Boris <boris.bbk@gmail.com>
2013-08-26 08:32:25 +0300

    Now project on Rails 4.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameRoom::Application.config.secret_key_base = 'b75b58e9e33b398e41fd106969ec6fc9ddf0953ab74e07aceeff3a8ebb993ce7ae731a8972bbffb54dcfb8d032ca55e862e49662f5e102a016ec363bc592c3df'
